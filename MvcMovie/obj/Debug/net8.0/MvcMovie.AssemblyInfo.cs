498ff4565ad3c9f46629b9676bfbb155de35c0cb
dofelton <90722967+dofelton@users.noreply.github.com>
2025-03-21 21:08:58 +0000

    Redo

--- MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6eaf8baa760449be63981045cdf60a2b4d04233d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eeb9055bba468b287b25976a75b7cc0818041ae0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
