76345a2baffc9af4cdacab7cadd8dc1d4b7a0ff2
dofelton <90722967+dofelton@users.noreply.github.com>
2025-03-23 00:34:57 +0000

    W2 final on main

--- MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+345e9977a46fa9c34b6378ebe14b94bdd1ac3806")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+512347021a30f773c550f3c27c2be3a95cecfdfc")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
